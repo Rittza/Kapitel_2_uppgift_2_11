e1d95d25978290f341b8368c6a239ac0d8ff4fe1
E-Svensson <111729734+CarlHellborgsson@users.noreply.github.com>
2022-09-09 11:05:57 +0200

    Add project files.

---
+++ Kapitel_2_uppgift_2_11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kapitel_2_uppgift_2_11
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine( "Hur många meter hoppade alma?");
+            string Almatext = Console.ReadLine();
+            int alma = int.Parse(Almatext);
+            int Elin = alma + 1;
+            Console.WriteLine( "Elin hoppade " + Elin + " meter vilket innebär att hon vann med 1 meter "); 
+        }
+    }
+}
